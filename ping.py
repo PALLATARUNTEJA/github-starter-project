520bc7cdaea298c09c2414228914dd00c1a246e9
Palla Tarun teja <pallatarunteja333@gmail.com>
2026-07-22 14:42:33 +0530

    add my first github action bot

---
+++ ping.py
@@ -0,0 +1,5 @@
+import datetime
+
+current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print("🤖 Hello from your GitHub Actions Robot!")
+print(f"⏰ The script ran successfully at: {current_time} UTC")
